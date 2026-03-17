521f57dfa623096507ad50c94ba12da39e56967d
Daniblazer <danisticks2@gmail.com>
2026-03-17 17:42:13 +0100

    add prometheus for monitoring, thereby modifying the docker-compose.yml and co

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, {urlencoded, json} from 'express';
+import client from "prom-client"; 
 
 import path from 'path';
 import favicon from 'serve-favicon';
@@ -14,8 +15,9 @@ import { listTaskUI, editTaskUI } from './app/controllers/task.controller.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Prometheus initialization
+client.collectDefaultMetrics();
 
-const dbUrl = process.env.DB_URL;
 const port = process.env.PORT || 3000;
 
 // create express app
@@ -24,27 +26,21 @@ const app = express();
 import v1UserRouter from './app/routes/user.routes.js';
 import v1TaskRouter from './app/routes/task.routes.js';
 
-// favicon
+// middleware
 app.use(favicon(path.join(__dirname, 'app', 'public', 'img', 'favicon.ico')));
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(urlencoded({ extended: true }));
-
-// parse requests of content-type - application/json
 app.use(json());
 
 // settings for Ejs
 app.set('views', path.join(`${__dirname}/app/views`));
 app.set('view engine', 'ejs');
-
-// serving static files
 app.use(express.static(__dirname + '/app/public'));
 
 // Mongoose require
 import { connect } from 'mongoose';
 
-// Connecting to the database
-connect(dbUrl, {
+// Connecting to the database - HARDCODED FOR DOCKER
+connect("mongodb://mongo:27017/todo-app", {
   useNewUrlParser: true,
 }).then(() => {
   console.log('Successfully connected to the database');
@@ -53,24 +49,25 @@ connect(dbUrl, {
   process.exit();
 });
 
+// Prometheus Metrics Route
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 // define a simple route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Todo App.' });
 });
 
-// Require User routes
 app.use("/v1/users", v1UserRouter);
 app.use("/v1/tasks", v1TaskRouter);
 
-// Views for frontend
 app.get('/users-ui/', listUserUI);
 app.get('/users-ui/edit/:userId', editUserUI);
-
 app.get('/tasks-ui/user/:userId', listTaskUI);
 app.get('/tasks-ui/edit/:taskId', editTaskUI);
 
-
-// listen for requests
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
